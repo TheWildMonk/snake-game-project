44f4aba9b3486bf963c84a04a615e39db5dc84b3
Shuvo Nasir <shuvonasir028@gmail.com>
2021-07-23 22:21:03 +0600

    add scoreboard class to keep track of user's score

---
+++ scoreboard.py
@@ -0,0 +1,24 @@
+from turtle import Turtle
+
+ALIGNMENT = "center"
+FONT = ("raleway", 14, "normal")
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.total_score = 0
+        self.color("white")
+        self.speed("fastest")
+        self.penup()
+        self.goto(0, 270)
+        self.update_score()
+        self.hideturtle()
+
+    def update_score(self):
+        self.write(f"SCORE: {self.total_score}", align=ALIGNMENT, font=FONT)
+
+    def increase_score(self):
+        self.total_score += 1
+        self.clear()
+        self.update_score()
